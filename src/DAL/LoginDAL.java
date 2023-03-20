29d855097fd09d58d42004ff3df3ae471958cf52
Manh11082001 <82518631+Manh11082001@users.noreply.github.com>
2023-03-20 14:46:46 +0700

    login 20/03/2023

---
+++ src/DAL/LoginDAL.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAL;
+
+import DTO.LoginDTO;
+import GUI.LoginGUI;
+
+/**
+ *
+ * @author huynh
+ */
+public class LoginDAL {
+    private LoginGUI view;
+ 
+    public LoginDAL(LoginGUI view) {
+        this.view = view;
+    }
+ 
+    public void login() {
+    }
+ 
+}
