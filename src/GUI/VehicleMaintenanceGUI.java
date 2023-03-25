a5a467494b2db6995d2800bafc3384311480e492
UIT-21522042 <96830385+hang21522042@users.noreply.github.com>
2023-03-25 13:40:13 +0700

    update 24/05

--- src/GUI/VehicleMaintenanceGUI.java
+++ src/GUI/VehicleMaintenanceGUI.java
@@ -38,6 +38,7 @@ public class VehicleMaintenanceGUI extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel2 = new javax.swing.JPanel();
+        menuBar1 = new java.awt.MenuBar();
         jPanel1 = new javax.swing.JPanel();
         jLabel4 = new javax.swing.JLabel();
         btnBack = new javax.swing.JButton();
@@ -45,10 +46,18 @@ public class VehicleMaintenanceGUI extends javax.swing.JFrame {
         jLayeredPane1 = new javax.swing.JLayeredPane();
         Phieu = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        list = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        txtTenChuXe = new java.awt.TextField();
+        txtBienSo = new java.awt.TextField();
+        txtHieuXe = new java.awt.TextField();
+        txtDiaChi = new java.awt.TextField();
+        txtNgayTiepNhan = new java.awt.TextField();
+        txtSDT = new java.awt.TextField();
+        jButton1 = new javax.swing.JButton();
+        jLabel8 = new javax.swing.JLabel();
 
         jPanel2.setLayout(new java.awt.CardLayout());
 
@@ -85,52 +94,130 @@ public class VehicleMaintenanceGUI extends javax.swing.JFrame {
 
         jLayeredPane1.setLayout(new java.awt.CardLayout());
 
-        jLabel1.setText("Phieu");
+        jLabel1.setText("Tên chủ xe");
+
+        jLabel2.setText("Hiệu xe");
+
+        jLabel3.setText("Biển số");
+
+        jLabel5.setText("Địa chỉ");
+
+        jLabel6.setText("Số điên thoại");
+
+        txtTenChuXe.setText("textField1");
+        txtTenChuXe.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtTenChuXeActionPerformed(evt);
+            }
+        });
+
+        txtBienSo.setText("textField1");
+        txtBienSo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtBienSoActionPerformed(evt);
+            }
+        });
+
+        txtHieuXe.setText("textField1");
+        txtHieuXe.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtHieuXeActionPerformed(evt);
+            }
+        });
+
+        txtDiaChi.setText("textField1");
+        txtDiaChi.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtDiaChiActionPerformed(evt);
+            }
+        });
+
+        txtNgayTiepNhan.setText("textField1");
+        txtNgayTiepNhan.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtNgayTiepNhanActionPerformed(evt);
+            }
+        });
+
+        txtSDT.setText("textField1");
+        txtSDT.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtSDTActionPerformed(evt);
+            }
+        });
+
+        jButton1.setText("Import");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jLabel8.setText("Ngày tiếp nhận");
 
         javax.swing.GroupLayout PhieuLayout = new javax.swing.GroupLayout(Phieu);
         Phieu.setLayout(PhieuLayout);
         PhieuLayout.setHorizontalGroup(
             PhieuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(PhieuLayout.createSequentialGroup()
-                .addGap(52, 52, 52)
-                .addComponent(jLabel1)
-                .addContainerGap(866, Short.MAX_VALUE))
+                .addGap(47, 47, 47)
+                .addGroup(PhieuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel5)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel6)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel8)
+                    .addComponent(jButton1))
+                .addGap(20, 20, 20)
+                .addGroup(PhieuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(PhieuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addComponent(txtHieuXe, javax.swing.GroupLayout.PREFERRED_SIZE, 238, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(PhieuLayout.createSequentialGroup()
+                            .addComponent(txtTenChuXe, javax.swing.GroupLayout.PREFERRED_SIZE, 271, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGap(111, 111, 111)
+                            .addComponent(jLabel3)
+                            .addGap(23, 23, 23)
+                            .addComponent(txtBienSo, javax.swing.GroupLayout.PREFERRED_SIZE, 279, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(txtDiaChi, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(txtSDT, javax.swing.GroupLayout.PREFERRED_SIZE, 181, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtNgayTiepNhan, javax.swing.GroupLayout.PREFERRED_SIZE, 616, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(0, 75, Short.MAX_VALUE))
         );
         PhieuLayout.setVerticalGroup(
             PhieuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(PhieuLayout.createSequentialGroup()
-                .addGap(97, 97, 97)
-                .addComponent(jLabel1)
-                .addContainerGap(387, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, PhieuLayout.createSequentialGroup()
+                .addGap(61, 61, 61)
+                .addGroup(PhieuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1)
+                    .addComponent(txtTenChuXe, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3)
+                    .addComponent(txtBienSo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(39, 39, 39)
+                .addGroup(PhieuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel2)
+                    .addComponent(txtHieuXe, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(39, 39, 39)
+                .addGroup(PhieuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel5)
+                    .addComponent(txtDiaChi, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(41, 41, 41)
+                .addGroup(PhieuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(PhieuLayout.createSequentialGroup()
+                        .addGroup(PhieuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel6)
+                            .addComponent(txtSDT, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(41, 41, 41)
+                        .addComponent(txtNgayTiepNhan, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jLabel8))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 44, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addGap(87, 87, 87))
         );
 
         jLayeredPane1.add(Phieu, "card2");
 
-        list.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        jLabel2.setText("list");
-        list.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 190, -1, -1));
-
-        jLayeredPane1.add(list, "card3");
-
-        jPanel1.add(jLayeredPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(25, 130, 950, 500));
+        jPanel1.add(jLayeredPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(25, 130, 950, 480));
         jLayeredPane1.getAccessibleContext().setAccessibleParent(jLayeredPane1);
-
-        jButton3.setText("test");
-        jButton3.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
-            }
-        });
-        jPanel1.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 90, 230, 40));
-
-        jButton4.setText("phieu");
-        jButton4.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton4ActionPerformed(evt);
-            }
-        });
-        jPanel1.add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 90, 230, 40));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -159,17 +246,33 @@ public class VehicleMaintenanceGUI extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_btnBackActionPerformed
 
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-      //  JPanel test;
+    private void txtSDTActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtSDTActionPerformed
         // TODO add your handling code here:
-        switchPanels(list);
-    }//GEN-LAST:event_jButton3ActionPerformed
+    }//GEN-LAST:event_txtSDTActionPerformed
 
-    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-        //JPanel Phieu;
+    private void txtNgayTiepNhanActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtNgayTiepNhanActionPerformed
         // TODO add your handling code here:
-        switchPanels(Phieu);
-    }//GEN-LAST:event_jButton4ActionPerformed
+    }//GEN-LAST:event_txtNgayTiepNhanActionPerformed
+
+    private void txtDiaChiActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtDiaChiActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtDiaChiActionPerformed
+
+    private void txtHieuXeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtHieuXeActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtHieuXeActionPerformed
+
+    private void txtBienSoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtBienSoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtBienSoActionPerformed
+
+    private void txtTenChuXeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtTenChuXeActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtTenChuXeActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     public void switchPanels(JPanel panel)
     {
@@ -218,14 +321,23 @@ public class VehicleMaintenanceGUI extends javax.swing.JFrame {
     private javax.swing.JPanel Phieu;
     private javax.swing.JButton btnBack;
     private javax.swing.JButton btnExit;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel8;
     private javax.swing.JLayeredPane jLayeredPane1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
-    private javax.swing.JPanel list;
+    private java.awt.MenuBar menuBar1;
+    private java.awt.TextField txtBienSo;
+    private java.awt.TextField txtDiaChi;
+    private java.awt.TextField txtHieuXe;
+    private java.awt.TextField txtNgayTiepNhan;
+    private java.awt.TextField txtSDT;
+    private java.awt.TextField txtTenChuXe;
     // End of variables declaration//GEN-END:variables
 }
