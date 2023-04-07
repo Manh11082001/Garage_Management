a082e078dfcc14430c1d7bb6af24ee680aacb71c
Manh11082001 <82518631+Manh11082001@users.noreply.github.com>
2023-04-07 17:40:12 +0700

    Update Mạnh

--- src/GUI/BillGUI.java
+++ src/GUI/BillGUI.java
@@ -327,7 +327,7 @@ public class BillGUI extends javax.swing.JFrame {
         txtBienSo.setText("");
         txtEmail.setText("");
         txtHoTen.setText("");
-        txtNgayThuTien.setText("");
+        //txtNgayThuTien.setText("");
         txtSDT.setText("");
         txtThuTien.setText("");
     }
